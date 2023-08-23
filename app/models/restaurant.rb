c9f838e5e064ab4cf42e6de6fa27256edb50310d
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-08-23 11:58:54 +0200

    seed with 5 restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: %w[chinese italian japanese french belgian], message: "It's not a valid category"}
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts 'Destroying all restaurants...'
+Restaurant.destroy_all
+puts 'Done!'
+
+puts 'Creating 5 restaurants...'
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    category: %w[chinese italian japanese french belgian].sample,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164
+  )
+end
+puts 'Finished!'
